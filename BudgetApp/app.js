fa6eb7023a0afec34065002be5172cb972e94a52
kblanka <kovacsblanka06@gmail.com>
2018-08-23 16:11:21 +0200

    Refactoring with init()

--- BudgetApp/app.js
+++ BudgetApp/app.js
@@ -31,26 +31,36 @@ var UIController = (function() {
 //GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
 
-    var DOM = UICtrl.getDomstrings();
+    var setupEventListener = function() {
+        var DOM = UICtrl.getDomstrings();
+
+        document.querySelector(DOM.inputBtn).addEventListener("click", function() {
+            ctrlAddItem();
+        });
+        document.addEventListener("keypress", function(event) {
+            if (event.keyCode === 13 || event.which === 13){
+                ctrlAddItem();
+            }
+        });
+    };
 
     var ctrlAddItem = function() {
+
         //1. Get the field input data
         var input = UICtrl.getInput();
         console.log(input);
+
         //2. Add the item to the budget controller
         //3. Add the new item to the UI
         //4. Calculate the budget
         //5. Display the budget
     }
 
-    document.querySelector(DOM.inputBtn).addEventListener("click", function() {
-        ctrlAddItem();
-    });
-    document.addEventListener("keypress", function(event) {
-        if (event.keyCode === 13 || event.which === 13){
-            ctrlAddItem();
+    return {
+        init: function() {
+            setupEventListener();
         }
-    });
-
+    }
 })(budgetController, UIController);
 
+controller.init();
