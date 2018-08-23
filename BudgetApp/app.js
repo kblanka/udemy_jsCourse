610b76cb2f1ec9202745be7be10a00827b9ed872
kblanka <kovacsblanka06@gmail.com>
2018-08-23 15:24:25 +0200

    Added getInput

--- BudgetApp/app.js
+++ BudgetApp/app.js
@@ -6,20 +6,44 @@ var budgetController = (function() {
 //UI CONTROLLER
 var UIController = (function() {
 
+    var DOMstrings = {
+        inputType: ".add__type",
+        inputDescription: ".add__description",
+        inputValue: ".add__value",
+        inputBtn: ".add__btn"
+    }
+    
+    return {
+        getInput: function(){
+            return {
+                type: document.querySelector(DOMstrings.inputType).value, //"inc or "exp"
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value
+            };
+        },
+        getDomstrings: function(){
+            return DOMstrings;
+        }
+
+    };
 })();
 
 //GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
 
+    var DOM = UICtrl.getDomstrings();
+
     var ctrlAddItem = function() {
         //1. Get the field input data
+        var input = UICtrl.getInput();
+        console.log(input);
         //2. Add the item to the budget controller
         //3. Add the new item to the UI
         //4. Calculate the budget
         //5. Display the budget
     }
 
-    document.querySelector(".add__btn").addEventListener("click", function() {
+    document.querySelector(DOM.inputBtn).addEventListener("click", function() {
         ctrlAddItem();
     });
     document.addEventListener("keypress", function(event) {
@@ -29,3 +53,4 @@ var controller = (function(budgetCtrl, UICtrl) {
     });
 
 })(budgetController, UIController);
+
