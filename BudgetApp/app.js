66d7fad26be801973d6ea2eb55625b908c028a90
kblanka <kovacsblanka06@gmail.com>
2018-08-23 14:43:12 +0200

    Added EventListener

--- BudgetApp/app.js
+++ BudgetApp/app.js
@@ -1,25 +1,31 @@
+//BUDGET CONTROLLER
 var budgetController = (function() {
-    var x = 23;
-    var add = function(a) {
-        return x + a;
-    }
-    return {
-        publicTest : function(b) {
-            return (add(b));
-        }
-    }
+
 })();
 
+//UI CONTROLLER
 var UIController = (function() {
 
 })();
 
+//GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
-    var z = budgetCtrl.publicTest(5);
 
-    return {
-        anotherPublic: function() {
-            console.log(z);
-        }
+    var ctrlAddItem = function() {
+        //1. Get the field input data
+        //2. Add the item to the budget controller
+        //3. Add the new item to the UI
+        //4. Calculate the budget
+        //5. Display the budget
     }
+
+    document.querySelector(".add__btn").addEventListener("click", function() {
+        ctrlAddItem();
+    });
+    document.addEventListener("keypress", function(event) {
+        if (event.keyCode === 13 || event.which === 13){
+            ctrlAddItem();
+        }
+    });
+
 })(budgetController, UIController);
