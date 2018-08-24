ffe0060eaa7dddd55d4aae317fdd8d18d10c22ff
kblanka <kovacsblanka06@gmail.com>
2018-08-24 21:45:57 +0200

    Field will be empty after submitting new entry

--- BudgetApp/app.js
+++ BudgetApp/app.js
@@ -79,11 +79,9 @@ var UIController = (function() {
             var html, newHtml, element;
 
             if (type === "inc") {
-                console.log('inc');
                 element = DOMstrings.incomeContainer;
                 html = '<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'
             } else if (type === "exp") {
-                console.log('exp');
                 element = DOMstrings.expenseContainer;
                 html = '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'
             }
@@ -95,6 +93,19 @@ var UIController = (function() {
        
             //Insert the HTML into the DOM
             document.querySelector(element).insertAdjacentHTML("beforeend",newHtml);
+        },
+
+        clearFields: function() {
+            var fields, fieldsArr;
+            fields = document.querySelectorAll(DOMstrings.inputDescription + ", " + DOMstrings.inputValue);
+
+            fieldsArr = Array.prototype.slice.call(fields);
+
+            fieldsArr.forEach(function(current, index, array) {
+                current.value = ""; 
+            });
+
+            fieldsArr[0].focus();
         },
 
         getDomstrings: function(){
@@ -131,8 +142,11 @@ var controller = (function(budgetCtrl, UICtrl) {
         //3. Add the new item to the UI
         UICtrl.addListItem(newItem, input.type);
 
-        //4. Calculate the budget
-        //5. Display the budget
+        //4. Clear the fields
+        UICtrl.clearFields();
+
+        //5. Calculate the budget
+        //6. Display the budget
     }
 
     return {
