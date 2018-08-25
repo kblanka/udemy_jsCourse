2b13a779837f2d1b79693c1570d83fe1c47cd68e
kblanka <kovacsblanka06@gmail.com>
2018-08-25 19:22:25 +0200

    getInput() misteriously got deleted, but now it's okay

--- BudgetApp/app.js
+++ BudgetApp/app.js
@@ -13,6 +13,15 @@ var budgetController = (function() {
         this.value = value;
     };
 
+    var calculateTotal = function(type) {
+        var sum = 0;
+        data.allItems[type].forEach(function(c) {
+            sum += c.value;
+        });
+        data.totals[type] = sum;
+    };
+
+
     var data = {
         allItems: {  
             exp: [],
@@ -21,7 +30,9 @@ var budgetController = (function() {
         totals: {
             exp: 0,
             inc: 0
-        }
+        },
+        budget: 0,
+        percentage: -1
     };
 
     return {
@@ -47,6 +58,28 @@ var budgetController = (function() {
             //Return new item
             return newItem;
         },
+
+        calculateBudget: function() {
+            //Calculate total income and expenses
+            calculateTotal('exp');
+            calculateTotal('inc');
+
+            //Calculate the budget: income - expenses7
+            data.budget = data.totals.inc - data.totals.exp;
+
+            //Calculate the percentage of income that we spent
+            data.percentage = Math.round(data.totals.expenses / data.totals.inc * 100);
+        },
+
+        getBudget: function() {
+            return {
+                budget: data.budget,
+                totalInc: data.totals.inc,
+                totalExp: data.totals.exp,
+                percentage: data.percentage
+            }
+        },
+
         testing: function() {
             console.log(data);
         }
@@ -65,7 +98,7 @@ var UIController = (function() {
         incomeContainer: ".income__list",
         expenseContainer: ".expenses__list"
     }
-    
+
     return {
         getInput: function(){
             return {
@@ -132,13 +165,14 @@ var controller = (function(budgetCtrl, UICtrl) {
     };
 
     var updateBudget = function() {
-
         //1. Calculate the budget
+        budgetCtrl.calculateBudget();
 
         //2. Return the budget
+        var budget = budgetCtrl.getBudget();
 
         //3. Display the budget
-
+        console.log(budget);
     };
 
     var ctrlAddItem = function() {
